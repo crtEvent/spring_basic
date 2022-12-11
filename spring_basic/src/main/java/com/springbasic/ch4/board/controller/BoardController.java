bb5ab14af240233ee4528bc8d53e416561fdbe6a
crtEvent <crtevn@gmail.com>
2022-12-11 14:23:22 +0900

    Add board read page
    * Add board read page
     - board.jsp 추가
       게시글 읽기 페이지 추가
    
     - boardList.jsp 수정
       게시글 목록 페이지 수정
       게시글 title 클릭시 게시글 읽기 페이지도 이동하는 링크 추가
    
     - BoardController.read() 추가
       게시글 읽기 페이지로 이동하는 메서드

--- spring_basic/src/main/java/com/springbasic/ch4/board/controller/BoardController.java
+++ spring_basic/src/main/java/com/springbasic/ch4/board/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springbasic.ch4.board.domain.PageHandler;
 import com.springbasic.ch4.board.dto.BoardDto;
@@ -39,4 +40,21 @@ public class BoardController {
 		
 		return "boardList";
 	}
+	
+	@GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, Model m) {
+        try {
+            BoardDto boardDto = boardService.read(bno);
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addAttribute("page", page);
+            rattr.addAttribute("pageSize", pageSize);
+            return "redirect:/board/list";
+        }
+
+        return "board";
+    }
 }
